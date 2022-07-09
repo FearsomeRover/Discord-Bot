c863de35ff9991ea57402c294ff43f2f52db5c76
FearsomeRover <bujgergo@gmail.com>
2022-07-09 18:35:27 +0200

    Update saveroles.js

--- commands/saveroles.js
+++ commands/saveroles.js
@@ -1,13 +1,12 @@
 module.exports = {
-    name: 'saveroles',
-    description: 'Lists the roles of a member (not yet able of saving)',
-    status: false,
-    args: "",
-    execute(client, message, args){
-        let member = message.mentions.members.first();
-        member.roles.add(role)
-
-        
-        message.react('👍')
-    }
+  status: false,
+  name: 'saveroles',
+  description: 'Lists the roles of a member (not yet able of saving)',
+  args: "",
+  execute(client, message, args){
+    let member = message.mentions.members.first();
+    member.roles.add(role)
+      
+    message.react('👍')
+  }
 }
