3e7ad97d4ad7be8297924f41426178f622e08bb6
FearsomeRover <bujgergo@gmail.com>
2022-07-09 20:54:56 +0200

    Update ty.js

--- commands/ty.js
+++ commands/ty.js
@@ -10,19 +10,6 @@ module.exports = {
   args: "",
     execute(client, message){
       const exampleEmbed = new MessageEmbed()
-	    .setColor('#FF61
-const { MessageEmbed, MessageAttachment} = require('discord.js');
-
-
-const file = new MessageAttachment('./content/uwu_crop.png');
-
-module.exports = {
-  status: true,
-    name: 'ty',
-    description: 'Thank you daddy, works if you type "good bot"',
-  args: "",
-    execute(client, message){
-      const exampleEmbed = new MessageEmbed()
 	    .setColor('#FF6187')
 	    .setTitle('Thank you daddy')
                 
@@ -30,13 +17,5 @@ module.exports = {
         
         message.channel.send({ embeds: [exampleEmbed] , files:[file]});
 
-    }
-}
-87')
-	    .setTitle('Thank you daddy')
-          
-	    .setImage('attachment://uwu_crop.png')
-
-        message.channel.send({ embeds: [exampleEmbed] , files:[file]});
     }
 }
