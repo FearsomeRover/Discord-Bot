d575fbf7f47a2118478d5f4e21c2985957022ee6
FearsomeRover <bujgergo@gmail.com>
2022-07-09 18:35:50 +0200

    Update ty.js

--- commands/ty.js
+++ commands/ty.js
@@ -1,18 +1,41 @@
 const { MessageEmbed, MessageAttachment} = require('discord.js');
 
 
-const file = new MessageAttachment('uwu.png');
+const file = new MessageAttachment('./content/uwu_crop.png');
 
 module.exports = {
+  status: true,
     name: 'ty',
-    description: 'Thank you daddy',
-    status: false,
-    args: "",
-    execute(client, message, args){
-        const exampleEmbed = new MessageEmbed()
-	    .setColor('#ffffff')
+    description: 'Thank you daddy, works if you type "good bot"',
+  args: "",
+    execute(client, message){
+      const exampleEmbed = new MessageEmbed()
+	    .setColor('#FF61
+const { MessageEmbed, MessageAttachment} = require('discord.js');
+
+
+const file = new MessageAttachment('./content/uwu_crop.png');
+
+module.exports = {
+  status: true,
+    name: 'ty',
+    description: 'Thank you daddy, works if you type "good bot"',
+  args: "",
+    execute(client, message){
+      const exampleEmbed = new MessageEmbed()
+	    .setColor('#FF6187')
 	    .setTitle('Thank you daddy')
-	    .setImage('attachment://uwu.png')
+                
+	    .setImage('attachment://uwu_crop.png')
+        
+        message.channel.send({ embeds: [exampleEmbed] , files:[file]});
+
+    }
+}
+87')
+	    .setTitle('Thank you daddy')
+          
+	    .setImage('attachment://uwu_crop.png')
 
         message.channel.send({ embeds: [exampleEmbed] , files:[file]});
     }
